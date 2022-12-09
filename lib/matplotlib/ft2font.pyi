f7b7020a7312f52c03af843c4b2b77973419b78b
Kyle Sunden <git@ksunden.space>
2022-12-09 12:17:54 -0600

    A few extra hints/import

--- lib/matplotlib/ft2font.pyi
+++ lib/matplotlib/ft2font.pyi
@@ -1,3 +1,5 @@
+import os
+
 class FT2Font:
     def __init__(self, filename: str|os.PathLike, hinting_factor: int = ..., *, _fallback_list: list["FT2Font"]|None = ..., _kerning_factor: int = ...): ...
 
