c0ed1e8e8518880382757dc9f4e69ef36ed7d13f
Kyle Sunden <git@ksunden.space>
2022-12-14 16:45:40 -0600

    font_manager

--- lib/matplotlib/tests/test_font_manager.py
+++ lib/matplotlib/tests/test_font_manager.py
@@ -14,9 +14,10 @@ import pytest
 from matplotlib.font_manager import (
     findfont, findSystemFonts, FontEntry, FontProperties, fontManager,
     json_dump, json_load, get_font, is_opentype_cff_font,
-    MSUserFontDirectories, _get_fontconfig_fonts, ft2font,
-    ttfFontProperty, cbook)
+    MSUserFontDirectories, _get_fontconfig_fonts, ttfFontProperty)
 from matplotlib import pyplot as plt, rc_context
+from matplotlib import cbook
+from matplotlib import ft2font
 
 has_fclist = shutil.which('fc-list') is not None
 
